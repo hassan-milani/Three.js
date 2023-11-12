1640817c83cd3af2f40d8985e72347882cbc8da2
hassan-milani <hassanmilani2002@gmail.com>
2023-11-12 21:46:33 +0300

    epic: added transition on click

--- script.js
+++ script.js
@@ -12,6 +12,7 @@ const canvas = document.querySelector(".webgl");
  * Scene
  */
 const scene = new THREE.Scene();
+
 /**
  *  3D MODELS
  *
@@ -20,8 +21,10 @@ const scene = new THREE.Scene();
 const loader = new GLTFLoader();
 loader.load("./REY.glb", function (gltf) {
   //If the file is loaded, add it to the scene
-  gltf.scene.position.set(0, 0, 2);
-  scene.add(gltf.scene);
+  const mainScene = gltf.scene;
+  mainScene.name = "mainScene";
+  mainScene.position.set(0, 0, 2);
+  scene.add(mainScene);
 });
 
 loader.load("./Screen1.glb", function (gltf) {
@@ -44,7 +47,7 @@ const light = new THREE.PointLight(0xffffff, 100, 100);
 light.position.set(5, 3, 10);
 
 scene.add(light);
-
+console.log(scene);
 //pointer
 const pointer = new THREE.Vector2();
 
@@ -70,12 +73,21 @@ const camera = new THREE.PerspectiveCamera(
   100
 );
 camera.position.set(-3, 5, 15);
+
 scene.add(camera);
 
 // Controls
 const controls = new OrbitControls(camera, canvas);
 controls.enableDamping = true;
 
+controls.keys = {
+  LEFT: "ArrowLeft",
+  UP: "ArrowUp",
+  BOTTOM: "ArrowDown",
+  RIGHT: "ArrowRight",
+};
+
+controls.listenToKeyEvents(window);
 // Raycaster
 var raycaster = new THREE.Raycaster();
 
@@ -95,7 +107,7 @@ const resetHover = () => {
 
 const hoverObjects = () => {
   raycaster.setFromCamera(pointer, camera);
-  const intersects = raycaster.intersectObjects(scene.children);
+  const intersects = raycaster.intersectObjects(scene.children.slice(0, 5));
 
   if (intersects.length > 0) {
     for (let i = 0; i < intersects.length; i++) {
@@ -107,16 +119,59 @@ const hoverObjects = () => {
 
 const onClick = () => {
   raycaster.setFromCamera(pointer, camera);
+  console.log(camera.position);
   let intersects = raycaster.intersectObjects(scene.children);
+  console.log(controls.target, "00");
+  console.log(camera.position, "11");
   if (intersects.length > 0) {
-    const targetPosition = intersects[0].object.position.clone();
+    const targetPosition = intersects[0].object.name;
     console.log(targetPosition);
-    gsap.to(camera.position, {
-      x: targetPosition.x + 4,
-      y: targetPosition.y,
-      z: targetPosition.z, // Move the camera a bit away from the object
-      duration: 1,
-    });
+    switch (targetPosition) {
+      case "Cube034_1":
+        gsap.to(camera.position, {
+          x: 4.3,
+          y: 2.6,
+          z: 0.25, // Move the camera a bit away from the object
+          duration: 1,
+        });
+        gsap.to(controls.target, {
+          x: -0.06,
+          y: 2.47,
+          z: 0.029, // Move the camera a bit away from the object
+          duration: 1,
+        });
+        break;
+      case "Cube037_1":
+        gsap.to(camera.position, {
+          x: 2.4,
+          y: 0.9,
+          z: 0.95, // Move the camera a bit away from the object
+          duration: 1,
+        });
+        gsap.to(controls.target, {
+          x: -0.101,
+          y: 0.67,
+          z: 0.87, // Move the camera a bit away from the object
+          duration: 1,
+        });
+        break;
+      case "Cube036_1":
+        gsap.to(camera.position, {
+          x: 2.48,
+          y: 0.85,
+          z: -0.92, // Move the camera a bit away from the object
+          duration: 1,
+        });
+        gsap.to(controls.target, {
+          x: -0.054,
+          y: 0.741,
+          z: -0.87, // Move the camera a bit away from the object
+          duration: 1,
+        });
+        break;
+      default:
+        break;
+    }
 
     console.log("you clicked the object");
   }
@@ -125,7 +180,7 @@ const onClick = () => {
 const animate = () => {
   controls.update();
   resetHover();
-  // hoverObjects();
+
   raycaster.setFromCamera(pointer, camera);
   window.requestAnimationFrame(animate);
   renderer.render(scene, camera);
